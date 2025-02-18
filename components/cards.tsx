3178a246eac7cf4ea3dc942df98492c575653693
Priscilla <priscilladunyoh99@gmail.com>
2025-02-18 22:50:02 +0000

    no update

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,17 +1,19 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { CustomText } from "./texts";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const TodoCard = () => {
   return (
-    <View className="w-full h-fit rounded-2xl p-5 bg-gray-900/20">
-      <View className="flex-row items-center justify-center">
-        <Ionicons name="checkmark-circle" size={16} color="black" />
-        <CustomText className="text-gray-900 text-lg font-semibold ml-2">
-          Blurry Background
-        </CustomText>
-      </View>
+    <View className="w-full h-fit rounded-2xl p-5 bg-green-700">
+        <View className="flex flex-row">
+            <CustomText>hello</CustomText>
+            <CustomText>hi</CustomText>
+        </View>
+      <Ionicons name="checkmark-circle" size={20} color="black" />
+      <CustomText className="text-gray-900 text-lg font-semibold ml-2  ">
+        Blurry Background
+      </CustomText>
     </View>
   );
 };
